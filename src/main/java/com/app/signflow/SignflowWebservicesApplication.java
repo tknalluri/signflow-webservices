07797e55ae67376b618a89fb6718f53ea247010e
Tarun Nalluri <tknalluri@gmail.com>
2026-01-29 21:48:52 -0500

    initial Commit

---
+++ src/main/java/com/app/signflow/SignflowWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.app.signflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignflowWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignflowWebservicesApplication.class, args);
+	}
+
+}
